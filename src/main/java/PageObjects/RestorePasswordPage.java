d8f6275573c247a1399e2cbc263e6f9a0d9d2fc6
BykovskayaDV <dbykovskaya.db@gmail.com>
2022-06-06 22:49:03 +0300

    Add Login Page

--- src/main/java/PageObjects/RestorePasswordPage.java
+++ src/main/java/PageObjects/RestorePasswordPage.java
@@ -1,4 +1,19 @@
 package PageObjects;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
 public class RestorePasswordPage {
+    @FindBy(how = How.XPATH, using = ".//div[text()='Имя']")
+    private SelenideElement nameField; // поле имя
+    @FindBy(how = How.XPATH, using = ".//div[text()='Email']")
+    private SelenideElement emailField; // поле Email
+    @FindBy(how = How.XPATH, using = ".//div[text()='Пароль']")
+    private SelenideElement passwordField; // поле Пароль
+    @FindBy(how = How.CLASS_NAME, using = "input__container")
+    private SelenideElement signUp; //кнопка Зарегистрироваться
+    @FindBy(how = How.XPATH, using = ".//div[text()='Войти']")
+    private SelenideElement enter; // поле Войти
+
 }
